3e7457de812560937dc9788361e5e412a4e45a00
sal <116751394+nrthnlght@users.noreply.github.com>
2023-12-23 01:53:38 +0700

    Add files via upload

---
+++ tugas pemrograman.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    for(int i=1; i<=4; i++){
+        if(i%2 == 0){
+            number = 1;
+        }else{
+            number = 2;
+        }
+        for(int j=0; j<4-i; j++){
+            printf("\t");
+        }
+        for(int k=1; k<=(i*2)-1; k++){
+            printf("%d",number);
+            number = number + 2;
+            printf("\t");
+        }
+        printf("\n");
+    }
+}
+//maaf pak saya telat mengumpulkan, saya salah buka tugas jadi salah deadline
